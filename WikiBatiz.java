5bda1de682e98c7769e984b290d5614118faa4be
よし <jsoriaf2400@alumno.ipn.mx>
2025-11-30 17:54:02 -0600

    Se añadieron métodos de entrada y el método de impresión del titulo de inicio del programa

--- WikiBatiz.java
+++ WikiBatiz.java
@@ -4,5 +4,60 @@ import java.util.Scanner;
 public class WikiBatiz {
   static boolean activo = true;
   static boolean reinicio = false;
-}
 
+      public static int NumRango(String[] opciones, int minimo, int maximo) {
+        int numIngresado = LeerNum();
+        boolean bandera = (numIngresado < minimo) || (numIngresado > maximo);
+        while (bandera) {
+            Separacion();
+            System.out.println("Ingresa un valor valido segun el menu");
+            Separacion();
+            for (int i = 0; i < opciones.length; i++) {
+                System.out.println(opciones[i]);
+            }
+            Separacion();
+            numIngresado = LeerNum();
+            if (numIngresado >= minimo && numIngresado <= maximo) {
+                bandera = false;
+            }
+        }
+        Separacion();
+        return numIngresado;
+    }
+
+    public static int LeerNum() {
+        int num = 0;
+        Scanner leer = new Scanner(System.in);
+        try {
+            num = leer.nextInt();
+            return num;
+        } catch (InputMismatchException e) {
+            Separacion();
+            System.out.println("Error, no haz ingresado un numero");
+            leer.next();
+        }
+        return num;
+    }
+
+    public static void Separacion() {
+        for (int i = 0; i < 150; i++) {
+            System.out.print("_");
+        }
+        System.out.println('\n');
+    }
+
+    public static void ImprimirNombre() {
+        System.out.println(" ____________________________________________________________________________________________________________ ");
+        System.out.println("|                                                                                                            |");
+        System.out.println("|      :::       ::: ::::::::::: :::    ::: ::::::::::: :::::::::      ::: ::::::::::: ::::::::::: ::::: :::: |");
+        System.out.println("|     :+:       :+:     :+:     :+:   :+:      :+:     :+:    :+:   :+: :+:   :+:         :+:          :+:   |");
+        System.out.println("|    +:+       +:+     +:+     +:+  +:+       +:+     +:+    +:+  +:+   +:+  +:+         +:+         +:+     |");
+        System.out.println("|   +#+  +:+  +#+     +#+     +#++:++        +#+     +#++:++#+  +#++:++#++: +#+         +#+        +#+       |");
+        System.out.println("|  +#+ +#+#+ +#+     +#+     +#+  +#+       +#+     +#+    +#+ +#+     +#+ +#+         +#+       +#+         |");
+        System.out.println("|  #+#+# #+#+#      #+#     #+#   #+#      #+#     #+#    #+# #+#     #+# #+#         #+#      #+#           |");
+        System.out.println("|  ###   ###   ########### ###    ### ########### #########  ###     ### ###     ########### #########       |");
+        System.out.println("|                                                                                                            |");
+        System.out.println("|____________________________________________________________________________________________________________|");
+        System.out.println('\n');
+    }
+}

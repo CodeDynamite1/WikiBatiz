e1d83523d5cc60470a7c6f447926be49d7237c52
KiraySanchezHalid <hkirays2400@alumno.ipn.mx>
2026-01-10 22:04:00 -0600

    ahora si ya en esta se imprimen las tablas con la informacion correcta

--- src/CLIversion/Tramites.java
+++ src/CLIversion/Tramites.java
@@ -1,24 +1,36 @@
 package CLIversion;
 
 public class Tramites extends Menu {
-    private static GestorDeDatos opcTramites = new GestorDeDatos("tramites_opciones");
-    private static GestorDeDatos msjTramites = new GestorDeDatos("tramites_mensajes");
+
+    private static GestorDeDatos opcTramites =
+            new GestorDeDatos("tramites_opciones");
+    private static GestorDeDatos msjTramites =
+            new GestorDeDatos("tramites_mensajes");
 
     public static String[] opcionesTramites = opcTramites.leerDatos();
     public static String[] mensajeTramites = msjTramites.leerDatos();
 
-    public static void proceso() {
+    // 🔴 DEVUELVE true SI QUIERE REGRESAR AL MENÚ
+    public static boolean proceso() {
+
         ImprimirNombre();
         int mostrar = menu(opcionesTramites);
+
+        // OPCIÓN "REGRESAR AL MENÚ"
+        if (mostrar == opcionesTramites.length) {
+            return true;
+        }
+
         switch (mostrar) {
             case 0:
                 System.out.println("error");
                 break;
             default:
-                System.out.println(mensajeTramites[mostrar - 1]);
+                // Reemplazamos los \n literales por saltos de línea reales
+                System.out.println(mensajeTramites[mostrar - 1].replace("\\n", "\n"));
                 break;
         }
+
+        return false;
     }
-
-
 }

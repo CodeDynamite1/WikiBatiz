f67dc166de0e40a9bd8401230e6305bd67acd95a
KiraySanchezHalid <hkirays2400@alumno.ipn.mx>
2026-01-10 22:00:34 -0600

    implementacion del nombre

--- src/CLIversion/Eventos.java
+++ src/CLIversion/Eventos.java
@@ -1,8 +1,9 @@
 package CLIversion;
 
-public class Eventos extends Menu{
-    public static void proceso(){
-        System.out.println("Debidoo a la situacion del plantel, no hay eventos proximos");
-    }
+public class Eventos extends Menu {
 
+    public static void proceso() {
+        ImprimirNombre();
+        System.out.println("Debido a la situacion del plantel, no hay eventos proximos");
+    }
 }

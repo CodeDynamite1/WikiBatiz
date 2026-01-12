16e5e32d4c539e97bdb1340932b193cbf5967e0f
よし <jsoriaf2400@alumno.ipn.mx>
2026-01-12 15:57:14 -0600

    Ordenamiento del proyecto

--- src/CLIversion/Eventos.java
+++
@@ -1,9 +0,0 @@
-package CLIversion;
-
-public class Eventos extends Menu {
-
-
-    public static void proceso() {
-        System.out.println("Debido a la situacion del plantel, no hay eventos proximos");
-    }
-}

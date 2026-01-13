412a3eea5ed6ae8b8ff89132f38fa905c92343fc
KiraySanchezHalid <hkirays2400@alumno.ipn.mx>
2026-01-13 10:36:02 -0600

    Funcion para editar tablas

--- src/Tabla.java
+++ src/Tabla.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Tabla {
     private String[][] matriz;
@@ -10,11 +12,54 @@ public class Tabla {
         imprimirEncabezado(nombre);
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                System.out.printf("║%12s",matriz[i][j]);
+                System.out.printf("|%12s",matriz[i][j]);
             }
-            System.out.println("║");
+            System.out.println("|");
             separacion();
         }
+    }
+
+    public void editarTabla(){
+        System.out.println("Imprimiendo contenido de la tabla");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.printf("%12d", i);
+        }
+        System.out.print('\n');
+
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.printf("[%d]",i);
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("|%12s",matriz[i][j]);
+            }
+            System.out.println("|");
+            separacion();
+        }
+        System.out.print("Escribe la fila a editar");
+        int fila = LeerNum();
+        System.out.print("Escribe la columna a editar");
+        int columna = LeerNum();
+        System.out.println("Ahora escribe el nuevo contenido");
+        Scanner Scanner = new Scanner(System.in);
+        matriz[filas][columnas] = Scanner.nextLine();
+        System.out.println("Deseas editar otra linea");
+        System.out.println("1.SI 2.NO");
+        int eleccion = LeerNum();
+        if (eleccion == 1) editarTabla();
+        return;
+
+    }
+
+    public static int LeerNum() {
+        int num = 0;
+        Scanner leer = new Scanner(System.in);
+        try {
+            num = leer.nextInt();
+            return num;
+        } catch (InputMismatchException e) {
+            System.out.println("Error, no haz ingresado un numero");
+            leer.next();
+        }
+        return num;
     }
 
     public Tabla(String[][] arr, String nombrar) {
@@ -33,7 +78,7 @@ public class Tabla {
     public void separacion(){
         int largoTotal = (columnas * 13) + 1;
         for (int i = 0; i < largoTotal; i++) {
-            System.out.print("═");
+            System.out.print("-");
         }
         System.out.print('\n');
     }
